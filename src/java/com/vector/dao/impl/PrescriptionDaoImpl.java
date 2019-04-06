81f16a282c9c08da108e12207b10cdef131f609a
IMVector <2523511898@qq.com>
2019-04-06 22:41:18 +0800

    exam doctor part has been done basiclly

---
+++ src/java/com/vector/dao/impl/PrescriptionDaoImpl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vector.dao.impl;
+
+import com.vector.dao.PrescriptionDao;
+import com.vector.pojo.Prescription;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Vector
+ */
+@Repository
+public class PrescriptionDaoImpl extends BaseDaoImpl<Prescription>implements PrescriptionDao {
+
+    @Override
+    public Integer getListItemNumber(Object... params) {
+        String hql="select count(*) from Prescription";
+        return getListSize(hql);
+    }
+
+    @Override
+    public List<Prescription> getList(Integer currentPage, Object... params) {
+        String hql="from Prescription";
+        return getListPaginationByQuery(hql, currentPage);
+    }
+    
+}
